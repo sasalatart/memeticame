4b54bcdc52e14df62c6b5752a3048c6ca2e81126
Sebastián Salata R-T <SA.SalataRT@GMail.com>
2016-08-27 12:42:01 -0300

    add swipe tabs

--- app/src/main/java/cl/mecolab/memeticame/Activities/MainActivity.java
+++ app/src/main/java/cl/mecolab/memeticame/Activities/MainActivity.java
@@ -1,38 +1,37 @@
 package cl.mecolab.memeticame.Activities;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import cl.mecolab.memeticame.Fragments.ChatsFragment;
 import cl.mecolab.memeticame.Fragments.ContactsFragment;
 import cl.mecolab.memeticame.Models.User;
 import cl.mecolab.memeticame.R;
+import cl.mecolab.memeticame.Views.ViewPagerAdapter;
 
-public class MainActivity extends AppCompatActivity implements
-        ContactsFragment.OnContactSelected {
+public class MainActivity extends AppCompatActivity implements ContactsFragment.OnContactSelected {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        // If fragment is already added, replace it.
-        if (getSupportFragmentManager().findFragmentByTag(ContactsFragment.TAG) != null) {
-            transaction = transaction.replace(R.id.main_container,
-                    new ContactsFragment(), null);
-        } else {
-            transaction = transaction.add(R.id.main_container,
-                    new ContactsFragment(), ContactsFragment.TAG);
-        }
-        transaction.commit();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    /**
-     * Called when contact is selected on ContactsFragment.
-     *
-     * @param user Contact selected.
-     */
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ContactsFragment(), "Contacts");
+        adapter.addFragment(new ChatsFragment(), "Chats");
+        viewPager.setAdapter(adapter);
+    }
+
     @Override
     public void onContactSelected(User user) {
 
---
+++ app/src/main/java/cl/mecolab/memeticame/Fragments/ChatsFragment.java
@@ -0,0 +1,48 @@
+package cl.mecolab.memeticame.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cl.mecolab.memeticame.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatsFragment extends Fragment {
+
+    public ChatsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_chats, container, false);
+
+        setHasOptionsMenu(true);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.chats_fragment_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return false;
+    }
+}
