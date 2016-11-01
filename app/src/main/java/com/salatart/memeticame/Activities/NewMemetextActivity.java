621ecb0630fda4d40160907c294930ce70550e89
Sebastián Oliva <saoliva@uc.cl>
2016-11-01 00:02:06 -0300

    add notification sound
    also resolve the camera rotation issue

--- app/src/main/java/com/salatart/memeticame/Activities/NewMemetextActivity.java
+++ app/src/main/java/com/salatart/memeticame/Activities/NewMemetextActivity.java
@@ -61,6 +61,8 @@ public class NewMemetextActivity extends AppCompatActivity {
     private File audioFile;
 
     public static Bitmap RotateBitmap(Bitmap source, float angle) {
+        if(angle == 0)
+            return source;
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
@@ -98,17 +100,17 @@ public class NewMemetextActivity extends AppCompatActivity {
         createMeme();
     }
 
-    public void setImageFromCurrentUri() {
+    public void setImageFromCurrentUri(float rotateAngle) {
         try {
             Bitmap picture = MediaStore.Images.Media.getBitmap(getContentResolver(), mCurrentImageUri);
-            drawBitmap(picture);
+            drawBitmap(picture, rotateAngle);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void drawBitmap(Bitmap picture) {
-        mCanvas.drawBitmap((Bitmap.createScaledBitmap(RotateBitmap(picture, 90), mCanvas.getWidth(), mCanvas.getHeight(), false)));
+    public void drawBitmap(Bitmap picture, float rotateAngle) {
+        mCanvas.drawBitmap((Bitmap.createScaledBitmap(RotateBitmap(picture,rotateAngle), mCanvas.getWidth(), mCanvas.getHeight(), false)));
     }
 
     private void createMeme() {
@@ -248,6 +250,7 @@ public class NewMemetextActivity extends AppCompatActivity {
             File photoFile = FileUtils.createMediaFile(getApplicationContext(), "jpg", Environment.DIRECTORY_PICTURES);
             if (photoFile != null) {
                 mCurrentImageUri = FileProvider.getUriForFile(this, "com.salatart.memeticame.fileprovider", photoFile);
+                System.out.println("hola:" + mCurrentImageUri.toString());
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCurrentImageUri);
                 startActivityForResult(takePictureIntent, FilterUtils.REQUEST_IMAGE_CAPTURE);
             }
@@ -267,17 +270,17 @@ public class NewMemetextActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == FilterUtils.REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            setImageFromCurrentUri();
+            setImageFromCurrentUri(90);
         } else if (requestCode == FilterUtils.REQUEST_PICK_FILE && resultCode == RESULT_OK && data != null) {
             mCurrentImageUri = data.getData();
-            setImageFromCurrentUri();
+            setImageFromCurrentUri(0);
         } else if (requestCode == FilterUtils.REQUEST_PICK_PLAIN_MEME && resultCode == RESULT_OK) {
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
                         String imageUri = (String) data.getExtras().get(Memetext.PARCELABLE_KEY);
-                        drawBitmap(FileUtils.getBitmapFromURL(imageUri));
+                        drawBitmap(FileUtils.getBitmapFromURL(imageUri), 0);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
