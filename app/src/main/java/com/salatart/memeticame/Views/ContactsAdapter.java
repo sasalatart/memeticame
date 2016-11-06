ba5218dbdf39ee69631fea25492eb70aecdb65ee
Sebastián Salata R-T <SA.SalataRT@GMail.com>
2016-11-06 18:22:27 -0300

    add TextDrawable to contact list items

--- app/src/main/java/com/salatart/memeticame/Views/ContactsAdapter.java
+++ app/src/main/java/com/salatart/memeticame/Views/ContactsAdapter.java
@@ -5,8 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.salatart.memeticame.Models.User;
 import com.salatart.memeticame.R;
 
@@ -35,10 +38,19 @@ public class ContactsAdapter extends ArrayAdapter<User> {
         User user = mContacts.get(position);
 
         TextView nameView = (TextView) view.findViewById(R.id.contact_name);
-        TextView phoneView = (TextView) view.findViewById(R.id.contact_phone_number);
-
         nameView.setText(user.getName());
+
+        TextView phoneView = (TextView) view.findViewById(R.id.contact_phone_number);
         phoneView.setText(user.getPhoneNumber());
+
+        ImageView identificationView = (ImageView) view.findViewById(R.id.label_user_identification);
+        TextDrawable userIdentification = TextDrawable.builder()
+                .beginConfig()
+                .withBorder(5)
+                .toUpperCase()
+                .endConfig()
+                .buildRoundRect(user.getName().charAt(0) + "", ColorGenerator.MATERIAL.getColor(user.getName()), 10);
+        identificationView.setImageDrawable(userIdentification);
 
         return view;
     }
--- app/src/main/java/com/salatart/memeticame/Views/ContactsSelectAdapter.java
+++ app/src/main/java/com/salatart/memeticame/Views/ContactsSelectAdapter.java
@@ -9,6 +9,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.salatart.memeticame.Models.User;
 import com.salatart.memeticame.R;
 
@@ -37,27 +39,41 @@ public class ContactsSelectAdapter extends ArrayAdapter<User> {
         View view = convertView;
 
         if (view == null) {
-            view = mLayoutInflater.inflate(R.layout.list_item_contact_checkable, parent, false);
+            view = mLayoutInflater.inflate(R.layout.list_item_contact, parent, false);
         }
 
         User user = mContacts.get(position);
 
-        ImageView isCheckedView = (ImageView) view.findViewById(R.id.contact_is_selected);
         TextView nameView = (TextView) view.findViewById(R.id.contact_name);
+        nameView.setText(user.getName());
+
         TextView phoneView = (TextView) view.findViewById(R.id.contact_phone_number);
+        phoneView.setText(user.getPhoneNumber());
+
+        String identificationString = user.getName().charAt(0) + "";
+        int identificationColor = ColorGenerator.MATERIAL.getColor(user.getName());
+        if (User.isPresent(mSelectedContacts, user.getPhoneNumber())) {
+            identificationString = "\u2713";
+            identificationColor = Color.GRAY;
+        }
 
         if (User.isPresent(mDisabledContacts, user.getPhoneNumber())) {
             nameView.setTextColor(Color.GRAY);
             phoneView.setTextColor(Color.GRAY);
+            identificationColor = Color.GRAY;
         } else {
             nameView.setTextColor(Color.BLACK);
             phoneView.setTextColor(Color.BLACK);
         }
 
-        nameView.setText(user.getName());
-        phoneView.setText(user.getPhoneNumber());
-
-        isCheckedView.setVisibility(User.isPresent(mSelectedContacts, user.getPhoneNumber()) ? View.VISIBLE : View.INVISIBLE);
+        ImageView identificationView = (ImageView) view.findViewById(R.id.label_user_identification);
+        TextDrawable userIdentification = TextDrawable.builder()
+                .beginConfig()
+                .withBorder(5)
+                .toUpperCase()
+                .endConfig()
+                .buildRoundRect(identificationString, identificationColor, 10);
+        identificationView.setImageDrawable(userIdentification);
 
         return view;
     }
